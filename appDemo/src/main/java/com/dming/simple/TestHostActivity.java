cb4887f89db00d3210b35103bcdeb749fcf26ab7
区达明 <756213932@qq.com>
2019-06-19 09:45:21 +0800

    onSaveInstanceState是在onPause后调用， 但是在stop方法后才通过binder发送数据给AMS，由于用binder，通信数据有限制

--- appDemo/src/main/java/com/dming/simple/TestHostActivity.java
+++ appDemo/src/main/java/com/dming/simple/TestHostActivity.java
@@ -1,7 +1,9 @@
 package com.dming.simple;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -20,19 +22,17 @@ public class TestHostActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.enteralpha, R.anim.exitalpha);
         setContentView(R.layout.layout_test);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-                (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)  != PackageManager.PERMISSION_GRANTED ||
-                        checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)  != PackageManager.PERMISSION_GRANTED)) {
-            Toast.makeText(this,"弹出权限请求,动画异常",Toast.LENGTH_SHORT).show();
-            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},666);
-        }
-
         findViewById(R.id.btn_test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(TestHostActivity.this, TestHostActivity.this.getString(R.string.test_toast),Toast.LENGTH_SHORT).show();
-//                getContentResolver().regi
-//                Looper.getMainLooper().quit();
+//                Toast.makeText(TestHostActivity.this, TestHostActivity.this.getString(R.string.test_toast),Toast.LENGTH_SHORT).show();
+//                startActivity(new Intent(TestHostActivity.this, TestHostActivity2.class));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                        (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)  != PackageManager.PERMISSION_GRANTED ||
+                                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)  != PackageManager.PERMISSION_GRANTED)) {
+                    Toast.makeText(TestHostActivity.this,"弹出权限请求,动画异常",Toast.LENGTH_SHORT).show();
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},666);
+                }
             }
         });
     }
@@ -62,5 +62,31 @@ public class TestHostActivity extends AppCompatActivity {
 //                new Handler().postDelayed(this,1000);
 //            }
 //        },200);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        DLog.i("onPause----/---->");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        DLog.i("onRestoreInstanceState------->");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+//        Bitmap bitmap = Bitmap.createBitmap(5000,5000, Bitmap.Config.ARGB_8888);
+//        outState.putParcelable("bit",bitmap);
+        DLog.i("onSaveInstanceState----5/--->");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        DLog.i("onStop------->");
     }
 }
