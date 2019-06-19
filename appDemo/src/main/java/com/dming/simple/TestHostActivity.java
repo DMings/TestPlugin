ed5bbcc88ddd1768562b9c2820313603004f7989
区达明 <756213932@qq.com>
2019-06-19 19:26:12 +0800

    测试uri to path，在小米的文件下载存在隔离，无法拿到path,要拿就复制一份（曲线救国）

--- appDemo/src/main/java/com/dming/simple/TestHostActivity.java
+++ appDemo/src/main/java/com/dming/simple/TestHostActivity.java
@@ -1,15 +1,9 @@
 package com.dming.simple;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.os.Build;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.view.View;
-import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.dming.simple.utils.DLog;
@@ -21,21 +15,40 @@ public class TestHostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         overridePendingTransition(R.anim.enteralpha, R.anim.exitalpha);
         setContentView(R.layout.layout_test);
-
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+//                (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)  != PackageManager.PERMISSION_GRANTED ||
+//                        checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)  != PackageManager.PERMISSION_GRANTED)) {
+//            Toast.makeText(TestHostActivity.this,"弹出权限请求,动画异常",Toast.LENGTH_SHORT).show();
+//            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},666);
+//        }
         findViewById(R.id.btn_test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                Toast.makeText(TestHostActivity.this, TestHostActivity.this.getString(R.string.test_toast),Toast.LENGTH_SHORT).show();
 //                startActivity(new Intent(TestHostActivity.this, TestHostActivity2.class));
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-                        (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)  != PackageManager.PERMISSION_GRANTED ||
-                                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)  != PackageManager.PERMISSION_GRANTED)) {
-                    Toast.makeText(TestHostActivity.this,"弹出权限请求,动画异常",Toast.LENGTH_SHORT).show();
-                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},666);
-                }
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");//设置类型，我这里是任意类型，任意后缀的可以这样写。
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(intent, 1);
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                Uri uri = data.getData();
+                if (uri != null) {
+                    DLog.i("uri: " + uri.toString());
+                    String path = UriUtils.getFilePathFromUri(this, uri);
+                    DLog.i("path: " + path);
+                }
+            }
+        }
+    }
+
 
     @Override
     public void onEnterAnimationComplete() {
